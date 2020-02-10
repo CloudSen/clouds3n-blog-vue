99e8c14f36dde0e40f94414f2a942efdaaed873c
clouds3n <www.402645063@gmail.com>
2020-02-10 23:59:01 +0800

    style(main): style code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-import vuetify from './plugins/vuetify'
-import router from './router'
-import store from './store'
+import vuetify from '@/plugins/vuetify'
+import router from '@/router/router'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 
