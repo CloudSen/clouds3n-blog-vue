cb5bf44dda2e225ef7e8375619f134d8ea0792ac
clouds3n <www.402645063@gmail.com>
2020-02-10 20:16:31 +0800

    feat(plugins): 添加md图标

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+// 确保使用了css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  // 使用 Material Design Icons
+  icons: {
+    iconfont: 'md',
+  },
 })
