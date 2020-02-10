366e47bba9c6ae45a216f2565c4dfb1143cdf8d8
clouds3n <www.402645063@gmail.com>
2020-02-10 23:57:45 +0800

    refactor(vue): 使用devtool

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
+  transpileDependencies: [
     "vuetify"
-  ]
+  ],
+  configureWebpack: {
+    // 使用VS Code调试代码需要的配置
+    // 见官方文档：https://cn.vuejs.org/v2/cookbook/debugging-in-vscode.html
+    devtool: 'source-map',
+  }
 }
