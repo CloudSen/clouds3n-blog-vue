8e433df41b8c4def29450340ae80c4d79c6c17c6
clouds3n <www.402645063@gmail.com>
2020-02-12 18:42:44 +0800

    refactor(views): 将测试数据传入终端组件

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>welcome</div>
+  <fragment>
+    <Terminal :data="this.terminalData"></Terminal>
+  </fragment>
 </template>
 
 <script>
+import Terminal from '@/components/terminal/Terminal'
+import testTerminalData from '@/testData/terminalData'
+
 export default {
   name: 'welcome',
+  components: {
+    Terminal,
+  },
+  data: () => ({
+    terminalData: {},
+  }),
+  methods: {
+    init () {
+      console.debug(`test terminal data: ${JSON.stringify(testTerminalData)}`)
+      this.terminalData = testTerminalData
+    },
+  },
+  created () {
+    this.init()
+  },
 }
 </script>
 
