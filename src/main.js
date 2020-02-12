621fb32bdb068647f7f54b22c7c2d26164434a98
clouds3n <www.402645063@gmail.com>
2020-02-12 14:10:10 +0800

    refactor(plugins): 重新配置fragment

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Fragment from 'vue-fragment'
 import App from './App'
 import vuetify from '@/plugins/vuetify'
 import router from '@/router/router'
 import store from '@/store/store'
 
 Vue.config.productionTip = false
+Vue.use(Fragment.Plugin)
 
 new Vue({
   vuetify,
