7612ef53403c67f38f50f87773e127cde79596cc
clouds3n <www.402645063@gmail.com>
2020-02-13 16:41:03 +0800

    refactor(router): 调整路由

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Welcome from '@/views/Welcome'
+import Home from '@/views/Home'
+
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,21 @@ const routes = [
   {
     path: '/',
     name: 'welcome',
+    title: 'welcome',
     component: Welcome,
+  },
+  {
+    path: '/home',
+    name: 'home',
+    title: 'home',
+    component: Home,
+    children: [
+      {
+        path: 'blog',
+        name: 'blog',
+        title: '个人博客',
+      },
+    ],
   },
 ]
 
