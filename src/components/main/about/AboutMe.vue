e90d85cb59f06a4aeb8a6215add0481e40953b43
clouds3n <www.402645063@gmail.com>
2020-02-13 23:19:49 +0800

    feat(components, testData, views): 添加关于页面

---
+++ src/components/main/about/AboutMe.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container
+    class="grey darken-4 grey--text text-lighten-4"
+    fill-height
+    grid-list-xs
+  >
+    <div
+      class="content grey darken-4 grey--text text-lighten-4 markdown-body"
+      v-html="renderContent"
+    ></div>
+  </v-container>
+</template>
+
+<script>
+import md from '@/plugins/markdown-it'
+import '@/assets/md-css/md-dark.css'
+import 'highlight.js/styles/xt256.css'
+import aboutMeData from '@/testData/aboutMeData'
+
+export default {
+  name: 'about-me',
+  data () {
+    return {
+      content: '',
+    }
+  },
+  methods: {
+    init () {
+      this.content = aboutMeData
+    },
+  },
+  computed: {
+    renderContent () {
+      return md.render(this.content)
+    },
+  },
+  created () {
+    this.init()
+  },
+}
+</script>
+
+<style scoped>
+.content {
+  height: 100%;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <AboutMe></AboutMe>
+</template>
+
+<script>
+import AboutMe from '@/components/main/about/AboutMe'
+
+export default {
+  name: 'about',
+  components: {
+    AboutMe,
+  },
+}
+</script>
+
+<style>
+</style>
