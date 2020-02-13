d5f9b6ec5de9b0f1b06cecbfa11a2a1af4260f34
clouds3n <www.402645063@gmail.com>
2020-02-13 21:41:10 +0800

    refactor(views): 添加嵌入显示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <fragment>
     <Header></Header>
     <Drawer></Drawer>
+    <Main></Main>
   </fragment>
 </template>
 
 <script>
 import Header from '@/components/header/Header'
 import Drawer from '@/components/aside/NavigationDrawer'
+import Main from '@/components/main/MainContent'
 
 export default {
   name: 'home',
   components: {
     Header,
     Drawer,
+    Main,
   },
 }
 </script>
