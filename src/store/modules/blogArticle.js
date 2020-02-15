6478162e36697ed87282272b836f80df50602686
clouds3n <www.402645063@gmail.com>
2020-02-15 23:46:55 +0800

    feat(store): vuex添加文章模块

---
+++ src/store/modules/blogArticle.js
@@ -0,0 +1,44 @@
+const state = {
+  articleListPage: {
+    size: 12,
+    current: 1,
+    total: 0,
+    pages: 0,
+  },
+  articleListCards: [],
+}
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {
+  updateArticleListPage ($state, payload) {
+    const STATE = $state
+    console.debug(`VUEX article list page update payload ${JSON.stringify(payload)}`)
+    STATE.articleListPage = { ...STATE.articleListPage, ...payload }
+    console.debug(`VUEX article list page updated to ${JSON.stringify(STATE.articleListPage)}`)
+  },
+  updateArticleListCards ($state, payload) {
+    const STATE = $state
+    STATE.articleListCards = payload
+  },
+  resetArticleListPage ($state) {
+    const STATE = $state
+    STATE.articleListPage = {
+      size: 12,
+      current: 1,
+      total: 0,
+      pages: 0,
+    }
+  },
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import navDrawer from '@/store/modules/navDrawer'
+import blogArticle from '@/store/modules/blogArticle'
 
 Vue.use(Vuex)
 
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   },
   modules: {
     navDrawer,
+    blogArticle,
   },
 })
