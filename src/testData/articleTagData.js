95447c2fab46cd630a573cb9c5e5d56aa76e32c5
clouds3n <www.402645063@gmail.com>
2020-02-16 16:47:58 +0800

    feat(testData): 添加标签测试数据

---
+++ src/testData/articleTagData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    uuid: '1',
+    name: 'JS',
+    color: 'amber lighten-2',
+  },
+  {
+    uuid: '2',
+    name: 'java',
+    color: 'blue lighten-1',
+  },
+  {
+    uuid: '3',
+    name: 'ElasticSearch',
+    color: 'amber darken-2',
+  },
+  {
+    uuid: '4',
+    name: 'GoLang',
+    color: 'green darken-2',
+  },
+  {
+    uuid: '5',
+    name: 'VUE',
+    color: 'lime darken-1',
+  },
+  {
+    uuid: '6',
+    name: 'Vuetify',
+    color: 'indigo lighten-1',
+  },
+]
