6071f6a520114068f0ebed4d13ec54f0963c921e
clouds3n <www.402645063@gmail.com>
2020-02-16 16:48:32 +0800

    feat(components): 添加有进度的图片加载组件

---
+++ src/components/common/ProgressImg.vue
@@ -0,0 +1,37 @@
+<template>
+  <fragment>
+    <v-img
+      :aspect-ratio="imgInfo.aspectRatio"
+      :src="imgInfo.src"
+      lazy-src="../../assets/lazy_loading.png"
+    >
+      <template v-slot:placeholder>
+        <v-row
+          align="center"
+          class="fill-height ma-0"
+          justify="center"
+        >
+          <v-progress-circular
+            color="grey lighten-5"
+            indeterminate
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
+  </fragment>
+</template>
+
+<script>
+export default {
+  name: 'progress-img',
+  props: {
+    imgInfo: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/common/ProgressImgWithCardTitle.vue
@@ -0,0 +1,40 @@
+<template>
+  <fragment>
+    <v-img
+      :class="imgInfo.class"
+      :src="imgInfo.src"
+      aspect-ratio="16/9"
+      height="250px"
+      lazy-src="../../assets/lazy_loading.png"
+    >
+      <v-card-title>{{imgInfo.title}}</v-card-title>
+      <template v-slot:placeholder>
+        <v-row
+          align="center"
+          class="fill-height ma-0"
+          justify="center"
+        >
+          <v-progress-circular
+            color="grey lighten-5"
+            indeterminate
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
+  </fragment>
+</template>
+
+<script>
+export default {
+  name: 'progress-img-card-title',
+  props: {
+    imgInfo: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+</style>
