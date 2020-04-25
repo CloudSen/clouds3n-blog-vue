6c94c045bc4cab50da6252a035e3985e2485c707
clouds3n <www.402645063@gmail.com>
2020-04-25 23:09:07 +0800

    refactor(all): 更改网站图标和名字，添加seo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Fragment from 'vue-fragment'
-import App from './App'
 import vuetify from '@/plugins/vuetify'
 import router from '@/router/router'
 import store from '@/store/store'
+import App from './App'
 
 Vue.config.productionTip = false
 Vue.use(Fragment.Plugin)
